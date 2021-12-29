a8a5f630e55e0cc92a2e71f0f666e4c9d2113406
Irene <irenemanningm@gmail.com>
2021-12-29 15:11:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRecord) {
+    return employeeRecord.map((employee) => {
+        return createEmployeeRecord(employee)
+    }) 
+}
+
+function createTimeInEvent(employeeRecord) {
+    
+    return employeeRecord.map((employee) => {
+        return employee.timeInEvents.push(
+        {
+            type: "TimeIn",
+            hour: hour,
+            date: date
+        })
+    })
+}
